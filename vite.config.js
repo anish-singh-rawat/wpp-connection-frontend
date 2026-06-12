59f0b4f3fd2da5a6419238cdda8fe7e3b5fc0861
anish-singh-rawat <www.computernetwork@gmail.com>
2026-06-12 13:53:43 +0530

    fix : update the vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  //   plugins: [
-  //   react({
-  //     babel: {
-  //       plugins: [['babel-plugin-react-compiler']],
-  //     },
-  //   }),
-  // ],
   plugins: [react()],
-  preview: {
-    host: "0.0.0.0",
-    allowedHosts: [
-      "digiwppconnect-frontend.digibysr.in",
-      "indianbaazaar.com",
-      "www.indianbaazaar.com"
-    ]
-  }
-});
+})
